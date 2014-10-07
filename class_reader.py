fb6ac833c57a5a9db75fca75527e7494b117e50c
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2014-10-07 21:28:23 +0200

    working class representation

---
+++ class_reader.py
@@ -0,0 +1,12 @@
+
+
+class ClassReader:
+    
+    @staticmethod
+    def read(filename):
+        pass
+
+
+if __name__ == '__main__':
+    classes = ClassReader.read('test.classes')
+    print classes
