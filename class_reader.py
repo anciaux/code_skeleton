eb0ef14e96e626690ccf572a7c40c4cceb9abdbe
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2014-10-08 09:49:13 +0200

    advancing

--- class_reader.py
+++ class_reader.py
@@ -1,12 +1,27 @@
-
+import re
+from class_decriptor import ClassDescriptor
 
 class ClassReader:
     
-    @staticmethod
-    def read(filename):
-        pass
+    def read(self,filename):
+        f = open(filename,'r')
+        for line in f:
+            self.readline(line)
+
+    def readline(self,line):
+        line = line.split('#')[0]
+        line = line.strip()
+        if line == "": return
+        m = re.match(r'class (.*)',line)
+        print line
+        if m: 
+            print line
+            print m.group(1)
+            
+        
 
 
 if __name__ == '__main__':
-    classes = ClassReader.read('test.classes')
+    cls_reader = ClassReader()
+    classes = cls_reader.read('test.classes')
     print classes
