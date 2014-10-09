f9109577a1f232f7be64857ad10b326c6d9d2dac
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2014-10-09 09:34:16 +0200

    cpp dumper

--- class_decriptor.py
+++ class_decriptor.py
@@ -21,6 +21,7 @@ class Method:
             sstr += _type + " " + k + ", "
         sstr += _type + " " + k + ")"
         return sstr
+
 class Member:
     def __init__(self,name,_type,encapsulation):
         self.name = name
@@ -35,29 +36,42 @@ class ClassDescriptor:
 
     def __init__(self,name):
         self.name = name 
-        self.members = []
-        self.methods = []
+        self.members = {'private':{},'public':{},'protected':{}}
+        self.methods = {'private':{},'public':{},'protected':{}}
 
-    
     def addMethod(self,name,args,ret,encapsulation,virtual):
-        self.methods.append(Method(name,args,ret,encapsulation,virtual))
+        new_method = Method(name,args,ret,encapsulation,virtual)
+        if name not in self.methods[encapsulation]: self.methods[encapsulation][name] = []
+        self.methods[encapsulation][name].append(new_method)
 
     def addMember(self,name,_type,encapsulation):
-        self.members.append(Member(name,_type,encapsulation))
+        new_member = Member(name,_type,encapsulation)
+        self.members[encapsulation][name] = new_member
+
+    def getMembers(self,encapsulation=None):
+        return self.members[encapsulation]
+
+    def getMethods(self,encapsulation=None):
+        return self.methods[encapsulation]
 
     def __str__(self):
         sstr = "Class " + self.name + "\n"
         sstr += "Methods:\n"
-        for m in self.methods:
-            sstr += str(m) + "\n"
+        for encaps,meths in self.methods.iteritems():
+            sstr += encaps + ":\n"
+            for name,m_list in meths.iteritems():
+                for m in m_list:
+                    sstr += str(m) + "\n"
         sstr += "\n"
         sstr += "Members:\n"
-        for m in self.members:
-            sstr += str(m) + "\n"       
+        for encaps,membs in self.members.iteritems():
+            sstr += encaps + ":\n"
+            for name,m in membs.iteritems():
+                sstr += str(m) + "\n"       
         return sstr
         
 if __name__ == '__main__':
     my_class = ClassDescriptor('dummy')
-    my_class.addMethod('compute',[('int','arg1'),('double','arg2')],'bool','public')
+    my_class.addMethod('compute',[('int','arg1'),('double','arg2')],'bool','public','')
     my_class.addMember('res','double','private')
     print my_class
