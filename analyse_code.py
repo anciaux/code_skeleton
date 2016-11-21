0553be75c33c23988dc8028d292cc384dfd30ceb
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2016-11-21 11:27:25 +0100

    linted format

--- analyse_code.py
+++ analyse_code.py
@@ -1,48 +1,67 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import subprocess,os,sys
-import class_decriptor as cd
-tmp_directory = '/tmp'
-from class_dumper_classes import ClassDumperClasses
+""" Module in charge of analysing a code """
 
 ################################################################
+import os
+import argparse
 import pygccxml.parser as gccparser
 from pygccxml import declarations
+import class_decriptor as cd
+from class_dumper_classes import ClassDumperClasses
+################################################################
+TMP_DIRECTORY = '/tmp'
+################################################################
 
+
+def clean_name(_name, **kwargs):
+    _name = str(_name)
+    if ('template' in kwargs) and kwargs['template'] is False:
+        _name = declarations.templates.name(str(_name))
+    if ('namespace' in kwargs) and kwargs['namespace'] is False:
+        namespace = _name.split('::')
+        if len(namespace) > 1:
+            _name = namespace[-1]
+    _name = str(_name)
+    return _name
 ################################################################
-def cleanName(n,**kwargs):
-    n = str(n)
-    if ('template' in kwargs) and kwargs['template'] is False: 
-        n = declarations.templates.name(str(n))
-    if ('namespace' in kwargs) and kwargs['namespace'] is False: 
-        ns = n.split('::')
-        if len(ns) > 1: n = ns[-1]
-    n = str(n)
-    return n
-################################################################
-def analyzeFile(fnames,include_paths=None,cflags=None,class_cache={},dec_dir=None,required_namespace=None,**kwargs):
-    if type(fnames) is not list: fnames = [fnames]
-#    print fnames
-    #parsing source file
-    #configure GCC-XML parser
-    #print include_paths
-    if cflags is None: cflags = ''
-    config = gccparser.gccxml_configuration_t( include_paths=include_paths,cflags=cflags, ignore_gccxml_output=True)
+
+def analyzeFile(fnames, include_paths=None, cflags=None, class_cache={},
+                dec_dir=None, required_namespace=None, **kwargs):
+
+    if not isinstance(fnames, list):
+        fnames = [fnames]
+
+    # print fnames
+    # parsing source file
+    # configure GCC-XML parser
+    # print include_paths
+
+    if cflags is None:
+        cflags = ''
+
+    config = gccparser.gccxml_configuration_t(include_paths=include_paths,
+                                              cflags=cflags,
+                                              ignore_gccxml_output=True)
     try:
-        decls = gccparser.parse( fnames, config)
-    except Exception as e:
+        decls = gccparser.parse(fnames, config)
+    except Exception as ex:
         print "Could not parse files '{0}'".format(fnames)
-        print e
+        print ex
         raise
-        return class_cache
-    
-    global_ns = declarations.get_global_namespace( decls )
-    if required_namespace is not None:
-        try: required_namespace = global_ns.namespace(required_namespace)
-        except: return class_cache
-    else: required_namespace = global_ns
+        # return class_cache
 
-        
+    global_ns = declarations.get_global_namespace(decls)
+    if required_namespace is not None:
+        try:
+            required_namespace = global_ns.namespace(required_namespace)
+        except Exception as ex:
+            return class_cache
+    else:
+        required_namespace = global_ns
+
+
     for class_ in required_namespace.classes():
         if dec_dir is not None:
             dec_file = declarations.declaration_files(class_)
@@ -50,110 +69,135 @@ def analyzeFile(fnames,include_paths=None,cflags=None,class_cache={},dec_dir=Non
             #print list(dec_file)
             #print os.path.abspath(dec_dir)
             #print [os.path.dirname(f) for f in dec_file]
-            flags = [os.path.abspath(dec_dir) == os.path.abspath(os.path.dirname(f)) for f in dec_file]
+            flags = [os.path.abspath(dec_dir) == os.path.abspath(os.path.dirname(f))
+                     for f in dec_file]
             flag = False
-            for f in flags: flag |= f
+            for _flag in flags:
+                flag |= _flag
             #print flags
             #print flag
-            if not flag: continue 
-            
+            if not flag:
+                continue
+
 
         print class_
         print class_.name
-        class_.name = cleanName(class_.name,**kwargs)
-        inheritance = [cleanName(base.related_class.name,**kwargs) for base in class_.bases]
+        class_.name = clean_name(class_.name, **kwargs)
+        inheritance = [clean_name(base.related_class.name, **kwargs) for base in class_.bases]
         #print inheritance
-        if inheritance == []: inheritance = None
-        c = cd.ClassDescriptor(class_.name,inheritance=inheritance)
-
+        if inheritance == []:
+            inheritance = None
+        c = cd.ClassDescriptor(class_.name, inheritance=inheritance)
 
         for memb in class_.vars(allow_empty=True):
             name = memb.name
             static = ""
-            if memb.type_qualifiers.has_static: static = 'static'
-            _type = cleanName(memb.type,**kwargs)
+            if memb.type_qualifiers.has_static:
+                static = 'static'
+            _type = clean_name(memb.type, **kwargs)
             encapsulation = memb.access_type
-            c.addMember(name,_type,encapsulation,static,"")            
+            c.addMember(name, _type, encapsulation, static, "")
 
         for cons in class_.constructors():
             name = cons.name
             static = ""
             const = cons.has_const
             virtual = cons.virtuality
-            if virtual == 'not virtual': virtual = ''
+            if virtual == 'not virtual':
+                virtual = ''
 
             ret = ""
             encapsulation = cons.access_type
             args = cons.arguments
-            
-            args = [(cleanName(a.type,**kwargs),a.name) for a in args]
-            c.addMethod(name,args,ret,encapsulation,virtual,static,const,"")
 
-        for foo in class_.member_functions(allow_empty=True):
-            name = foo.name
+            args = [(clean_name(a.type, **kwargs), a.name) for a in args]
+            c.addMethod(name, args, ret, encapsulation, virtual, static, const, "")
+
+        for func in class_.member_functions(allow_empty=True):
+            name = func.name
             static = ""
-            if foo.has_static: static = 'static'
-            const = foo.has_const
-            virtual = foo.virtuality
-            if virtual == 'not virtual': virtual = ''
+            if func.has_static:
+                static = 'static'
+            const = func.has_const
+            virtual = func.virtuality
+            if virtual == 'not virtual':
+                virtual = ''
 
-            ret = cleanName(foo.return_type,**kwargs)
-            encapsulation = foo.access_type
-            args = foo.arguments
-            
-            args = [(cleanName(a.type,**kwargs),a.name) for a in args]
-            c.addMethod(name,args,ret,encapsulation,virtual,static,const,"")
+            ret = clean_name(func.return_type, **kwargs)
+            encapsulation = func.access_type
+            args = func.arguments
+
+            args = [(clean_name(a.type, **kwargs), a.name) for a in args]
+            c.addMethod(name, args, ret, encapsulation, virtual, static, const, "")
         class_cache[class_.name] = c
     return class_cache
-    
+
 
 ################################################################
-def analyzeFiles(dirname,extension_list = ['.hh','.hpp'],output=None,**kwargs):
+def analyze_files(dirname, extension_list=None, output=None, **kwargs):
 
-    if output is None: raise
+    if extension_list is None:
+        extension_list = ['.hh', '.hpp']
+        
+    if output is None:
+        raise Exception('output is not provided')
     read_classes = {}
-    if os.path.isfile(dirname): files = [dirname]
+    if os.path.isfile(dirname):
+        files = [dirname]
     else:
         files = []
-        for f in os.listdir(dirname):
-            base,ext = os.path.splitext(f)
-            if ext in extension_list: files.append(os.path.join(dirname,f))
-            
-    if os.path.isfile(dirname): dirname = None
-    for f in files:
-        analyzeFile(f,class_cache=read_classes,dec_dir=dirname,**kwargs)
+        for filename in os.listdir(dirname):
+            dummy_base, ext = os.path.splitext(filename)
+            if ext in extension_list:
+                files.append(os.path.join(dirname, filename))
+
+    if os.path.isfile(dirname):
+        dirname = None
+    for filename in files:
+        analyzeFile(filename, class_cache=read_classes, dec_dir=dirname, **kwargs)
 
     print read_classes.keys()
-    
-    
+
+
     if output is not None:
         dumper_class = ClassDumperClasses(output)
-        classes = [c for k,c in read_classes.iteritems()]
+        classes = [c for dummy_k, c in read_classes.iteritems()]
         dumper_class.dump(classes=classes)
 
-    
-################################################################
-if __name__ == '__main__':
 
-    import argparse
+################################################################
+def main():
+
     parser = argparse.ArgumentParser(description='Code analyzer to produce .classes description')
-    parser.add_argument('--sources','-s', help='The directory where the sources are loacted',required=True)
-    parser.add_argument('--includes','-I', type=str,help='The needed includes')
-    parser.add_argument('--cflags','-f', type=str,help='The needed flags')
-    parser.add_argument('--template','-t', action='store_false',help='Remove templates from analysis')
-    parser.add_argument('--namespace','-n', action='store_false',help='Remove namespaces from analysis')
-    parser.add_argument('--output','-o', type=str,help='filename to store output')
-    parser.add_argument('--required_namespace','-rn', type=str,help='filter classes based on namespace')
-    
+    parser.add_argument('--sources', '-s',
+                        help='The directory where the sources are loacted',
+                        required=True)
+    parser.add_argument('--includes', '-I', type=str, help='The needed includes')
+    parser.add_argument('--cflags', '-f', type=str, help='The needed flags')
+    parser.add_argument('--template', '-t', action='store_false',
+                        help='Remove templates from analysis')
+    parser.add_argument('--namespace', '-n', action='store_false',
+                        help='Remove namespaces from analysis')
+    parser.add_argument('--output', '-o', type=str,
+                        help='filename to store output')
+    parser.add_argument('--required_namespace', '-rn', type=str,
+                        help='filter classes based on namespace')
+
 
     args = parser.parse_args()
     args = vars(args)
     src_dir = args['sources']
     inc_dirs = None
     cflags = args['cflags']
-    if cflags is not None: cflags = cflags.replace('\-','-')
+    if cflags is not None:
+        cflags = cflags.replace('\-','-')
     args['cflags'] = cflags
-    if args['includes'] is not None: inc_dirs = args['includes'].split(';')
-    
-    analyzeFiles(src_dir,include_paths=inc_dirs,**args)
-    
+    if args['includes'] is not None:
+        inc_dirs = args['includes'].split(';')
+
+    analyze_files(src_dir, include_paths=inc_dirs, **args)
+
+################################################################
+
+if __name__ == '__main__':
+    main()
--- merge_classes.py
+++ merge_classes.py
@@ -1,73 +1,79 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Module for merging class representations """
 
 ################################################################
-import os, re, sys
 import argparse
-from class_dumper    import ClassDumper
 from class_dumper_classes    import ClassDumperClasses
 from class_reader    import ClassReader
-from class_decriptor import Method
 ################################################################
 
-def merge(files,output):
-    print files 
+def merge(files, output_filename):
+
+    "takes a list of files and produce a single one"
+    print files
     classes = []
     for class_file in files:
         cls_reader = ClassReader()
         classes.append(cls_reader.read(class_file))
 
-    print len(classes)," files to merge"
+    print len(classes), " files to merge"
 
     merged_classes = {}
 
-    for _c in classes:
-        for c in _c:
+    for _class in classes:
+        for c in _class:
             name = c.name
-            if c.name not in merged_classes: merged_classes[c.name] = c
+            if c.name not in merged_classes:
+                merged_classes[c.name] = c
+
             else:
                 pclass = merged_classes[c.name]
-                for encaps,methods in c.methods.iteritems():
-                    for mname,method in methods.iteritems():
+                for encaps, methods in c.methods.iteritems():
+                    for mname, method in methods.iteritems():
                         if mname in pclass.methods[encaps]:
                             if not pclass.methods[encaps][mname] == method:
-                                print "Warming: ",((c.name,encaps,mname))
+                                print "Warming: ", ((c.name, encaps, mname))
                         else:
                             pclass.methods[encaps][mname] = method
 
-                for encaps,members in c.members.iteritems():
-                    for mname,member in members.iteritems():
+                for encaps, members in c.members.iteritems():
+                    for mname, member in members.iteritems():
                         if mname in pclass.members[encaps]:
                             if not pclass.members[encaps][mname] == member:
-                                print "Warming: ",((c.name,encaps,mname))
+                                print "Warming: ", ((c.name, encaps, mname))
                             else:
                                 pclass.members[encaps][mname] = member
 
-                        
+
 
     #for c in merged_classes:
     #    print merged_classes[c].name
 
 
-    dumper_class = ClassDumperClasses('merged_file.classes')
-    fout = open(dumper_class.output_file,'w')
-    for name,c in merged_classes.iteritems():
-        dumper_class.dumpClass(c,fout)
+    dumper_class = ClassDumperClasses('output_filename')
+    fout = open(dumper_class.output_file, 'w')
+    for name, _class in merged_classes.iteritems():
+        dumper_class.dumpClass(_class, fout)
 
 
 ################################################################
-if __name__ == '__main__':
 
+def main():
     parser = argparse.ArgumentParser(description='Classes descriptor merge')
     parser.add_argument('class_files', nargs='+', help='The class files to merge')
-    parser.add_argument('--output_file','-o' , help='The file where to put the classes description',required=True)
+    parser.add_argument('--output_file', '-o',
+                        help='The file where to put the classes description', required=True)
 
     args = parser.parse_args()
     args = vars(args)
-    
 
     class_files = args['class_files']
     output_file = args['output_file']
 
-    merge(class_files,output_file)
+    merge(class_files, output_file)
 
-    
+
+if __name__ == '__main__':
+    main()
